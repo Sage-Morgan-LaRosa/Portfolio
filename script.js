5714a8abb766b30ec20cf5a22892aacf6b346ee9
Sage Morgan-Larosa <samo4421@colorado.edu>
2025-01-21 16:17:46 -0700

    updated for exit ticket

--- script.js
+++ script.js
@@ -3,22 +3,7 @@ const photos = [
     'images/photo2.png',
     'images/photo3.png',
     'images/photo4.png',
-    'images/photo5.png',
-    'images/photo6.png',
-    'images/photo7.png',
-    'images/photo8.png',
-    'images/photo9.png',
-    'images/photo10.png',
-    'images/photo11.png',
-    'images/photo12.png',
-    'images/photo13.png',
-    'images/photo14.png',
-    'images/photo15.png',
-    'images/photo16.png',
-    'images/photo17.png',
-    'images/photo18.png',
-    'images/photo19.png',
-    'images/photo20.png'
+    'images/photo5.png'
 ];
 
 let currentIndex = 0;
